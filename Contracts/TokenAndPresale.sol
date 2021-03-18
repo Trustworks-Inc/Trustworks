c751d17eb6886d6ecf75ac9b3891e85a5b40e955
SonGokuBg <56365283+SonGokuBg@users.noreply.github.com>
2021-03-18 18:49:25 +0100

    Update TokenAndPresale.sol

--- Contracts/TokenAndPresale.sol
+++ Contracts/TokenAndPresale.sol
@@ -1,5 +1,4 @@
-pragma solidity ^0.7.0;
- 
+pragma solidity 0.7.6;
  
  
 interface IPCS {
@@ -66,7 +65,7 @@ interface IpancakeV2Factory {
 /**
  * @dev Interface of the bep20 standard as defined in the EIP.
  */
-interface Ibep20 {
+interface IBEP20 {
     /**
      * @dev Returns the amount of tokens in existence.
      */
@@ -148,11 +147,13 @@ abstract contract Context {
     }
 }
  
-contract TRUST is Context, Ibep20, ReentrancyGuard {
+contract Trustworks is Context, IBEP20, ReentrancyGuard {
     using SafeMath for uint256;
+    using SafeBEP20 for IBEP20;
+    using Address for address payable;
  
-    IPCS constant pancake =  IPCS(0x05fF2B0DB69458A0750badebc4f9e13aDd608C7F);
-    IpancakeV2Factory constant pancakeFactory = IpancakeV2Factory(0xBCfCcbde45cE874adCB698cC183deBcF17952812);
+    IPCS private constant pancake =  IPCS(0x05fF2B0DB69458A0750badebc4f9e13aDd608C7F);
+    IpancakeV2Factory private constant pancakeFactory = IpancakeV2Factory(0xBCfCcbde45cE874adCB698cC183deBcF17952812);
  
     mapping (address => uint256) private _balances;
  
@@ -160,34 +161,31 @@ contract TRUST is Context, Ibep20, ReentrancyGuard {
  
     uint256 private _totalSupply;
  
-    string private _name;
-    string private _symbol;
-    uint8 private _decimals;
+     string private constant _name = "Trustworks";
+     string private constant  _symbol = "TRUST";
+     uint8 private constant _decimals = 18;
  
     bool public isStopped = true;
     bool public isRefundEnabled  = false;
     bool public devClaimed = false;
     bool public moonMissionStarted = false;
  
-    bool justTrigger = false;
     bool transferPaused = true;
     
  
     address payable _owner;
     address payable multisig; // Address will be added after the multisig contract is deployed 
-    address payable deflect = 0x5ABBd94bb0561938130d83FdA22E672110e12528;
+    address payable constant deflect = 0x5ABBd94bb0561938130d83FdA22E672110e12528;
     address public pool;
  
-    uint256 public liquidityUnlock;
+    uint256 public immutable liquidityUnlock;
  
  
     uint256 public ethSent;
     uint256 constant tokensPerBNB = 500;
-    uint256 public lockedLiquidityAmount;
-    uint256 public refundTime; 
+    uint256 public immutable refundTime; 
     mapping(address => uint) ethSpent;
-    mapping(address => uint) bought;
- 
+
    modifier onlyOwner() {
         require(_owner == _msgSender(), "Ownable: caller is not the owner");
         _;
@@ -195,9 +193,7 @@ contract TRUST is Context, Ibep20, ReentrancyGuard {
  
     constructor ()  {
         _owner = _msgSender();
-        _name = "Trustworks";
-        _symbol = "TRUST";
-        _decimals = 18;
+        
         _totalSupply = 1000000 ether;
         refundTime = block.timestamp.add(30 days);
         liquidityUnlock = block.timestamp.add(365 days);
@@ -227,7 +223,7 @@ contract TRUST is Context, Ibep20, ReentrancyGuard {
         require(msg.value >= 0.1 ether, "You sent less than 0.1 BNB");
         require(msg.value <= 25 ether, "You sent more than 25 BNB");
         require(ethSent < 750 ether, "Hard cap reached");
-        require (msg.value.add(ethSent) <= 750 ether, "Hardcap will be reached");
+        require (msg.value.add(ethSent) <= 750 ether, "Hardcap will be exceeded");
         require(ethSpent[msg.sender].add(msg.value) <= 25 ether, "You cannot buy more");
         uint256 tokens = msg.value.mul(tokensPerBNB);
         require(balanceOf(address(this)) >= tokens, "Not enough tokens in the contract");
@@ -252,10 +248,12 @@ contract TRUST is Context, Ibep20, ReentrancyGuard {
        uint256 forDeflect = address(this).balance.mul(200).div(1000); // 20%
        uint256 forMultisig = address(this).balance.mul(425).div(1000); //42.4%
  
-       multisig.transfer(forMultisig);
-       deflect.transfer(forDeflect);
+       
        devClaimed = true;
- 
+       
+       multisig.sendValue(forMultisig);
+       deflect.sendValue(forDeflect);
+       
        moonMissionStart();
     }
  
@@ -266,7 +264,7 @@ contract TRUST is Context, Ibep20, ReentrancyGuard {
  
     function getRefund() external nonReentrant {
         require(msg.sender == tx.origin);
-        require(!justTrigger);
+        require(!moonMissionStarted);
         // To get refund it should be enabled by the owner OR 10 days had passed 
         require(isRefundEnabled || block.timestamp >= refundTime,"Cannot refund");
         address payable user = msg.sender;
@@ -313,19 +311,18 @@ contract TRUST is Context, Ibep20, ReentrancyGuard {
             isStopped = true;
  
         moonMissionStarted = true;
-        justTrigger = true;
    }
  
-    function recoverbep20(address tokenAddress, uint256 tokenAmount) public onlyOwner  {
-        require(block.timestamp >= liquidityUnlock);
-        Ibep20(tokenAddress).transfer(_owner, tokenAmount);
+    function recoverbep20(IBEP20 tokenAddress, uint256 tokenAmount) public onlyOwner  {
+        require(block.timestamp >= liquidityUnlock, "Can't withdraw yet");
+        tokenAddress.safeTransfer(_owner, tokenAmount);
     }
  
  
     /**
      * @dev Returns the name of the token.
      */
-    function name() public view returns (string memory) {
+    function name() public pure returns (string memory) {
         return _name;
     }
  
@@ -333,7 +330,7 @@ contract TRUST is Context, Ibep20, ReentrancyGuard {
      * @dev Returns the symbol of the token, usually a shorter version of the
      * name.
      */
-    function symbol() public view returns (string memory) {
+    function symbol() public pure returns (string memory) {
         return _symbol;
     }
  
@@ -350,7 +347,7 @@ contract TRUST is Context, Ibep20, ReentrancyGuard {
      * no way affects any of the arithmetic of the contract, including
      * {Ibep20-balanceOf} and {Ibep20-transfer}.
      */
-    function decimals() public view returns (uint8) {
+    function decimals() public pure returns (uint8) {
         return _decimals;
     }
  
@@ -660,5 +657,255 @@ library SafeMath {
     function mod(uint256 a, uint256 b, string memory errorMessage) internal pure returns (uint256) {
         require(b != 0, errorMessage);
         return a % b;
+    }
+}
+
+library Address {
+    /**
+     * @dev Returns true if `account` is a contract.
+     *
+     * [IMPORTANT]
+     * ====
+     * It is unsafe to assume that an address for which this function returns
+     * false is an externally-owned account (EOA) and not a contract.
+     *
+     * Among others, `isContract` will return false for the following
+     * types of addresses:
+     *
+     *  - an externally-owned account
+     *  - a contract in construction
+     *  - an address where a contract will be created
+     *  - an address where a contract lived, but was destroyed
+     * ====
+     */
+    function isContract(address account) internal view returns (bool) {
+        // This method relies on extcodesize, which returns 0 for contracts in
+        // construction, since the code is only stored at the end of the
+        // constructor execution.
+
+        uint256 size;
+        // solhint-disable-next-line no-inline-assembly
+        assembly { size := extcodesize(account) }
+        return size > 0;
+    }
+
+    /**
+     * @dev Replacement for Solidity's `transfer`: sends `amount` wei to
+     * `recipient`, forwarding all available gas and reverting on errors.
+     *
+     * https://eips.ethereum.org/EIPS/eip-1884[EIP1884] increases the gas cost
+     * of certain opcodes, possibly making contracts go over the 2300 gas limit
+     * imposed by `transfer`, making them unable to receive funds via
+     * `transfer`. {sendValue} removes this limitation.
+     *
+     * https://diligence.consensys.net/posts/2019/09/stop-using-soliditys-transfer-now/[Learn more].
+     *
+     * IMPORTANT: because control is transferred to `recipient`, care must be
+     * taken to not create reentrancy vulnerabilities. Consider using
+     * {ReentrancyGuard} or the
+     * https://solidity.readthedocs.io/en/v0.5.11/security-considerations.html#use-the-checks-effects-interactions-pattern[checks-effects-interactions pattern].
+     */
+    function sendValue(address payable recipient, uint256 amount) internal {
+        require(address(this).balance >= amount, "Address: insufficient balance");
+
+        // solhint-disable-next-line avoid-low-level-calls, avoid-call-value
+        (bool success, ) = recipient.call{ value: amount }("");
+        require(success, "Address: unable to send value, recipient may have reverted");
+    }
+
+    /**
+     * @dev Performs a Solidity function call using a low level `call`. A
+     * plain`call` is an unsafe replacement for a function call: use this
+     * function instead.
+     *
+     * If `target` reverts with a revert reason, it is bubbled up by this
+     * function (like regular Solidity function calls).
+     *
+     * Returns the raw returned data. To convert to the expected return value,
+     * use https://solidity.readthedocs.io/en/latest/units-and-global-variables.html?highlight=abi.decode#abi-encoding-and-decoding-functions[`abi.decode`].
+     *
+     * Requirements:
+     *
+     * - `target` must be a contract.
+     * - calling `target` with `data` must not revert.
+     *
+     * _Available since v3.1._
+     */
+    function functionCall(address target, bytes memory data) internal returns (bytes memory) {
+      return functionCall(target, data, "Address: low-level call failed");
+    }
+
+    /**
+     * @dev Same as {xref-Address-functionCall-address-bytes-}[`functionCall`], but with
+     * `errorMessage` as a fallback revert reason when `target` reverts.
+     *
+     * _Available since v3.1._
+     */
+    function functionCall(address target, bytes memory data, string memory errorMessage) internal returns (bytes memory) {
+        return functionCallWithValue(target, data, 0, errorMessage);
+    }
+
+    /**
+     * @dev Same as {xref-Address-functionCall-address-bytes-}[`functionCall`],
+     * but also transferring `value` wei to `target`.
+     *
+     * Requirements:
+     *
+     * - the calling contract must have an ETH balance of at least `value`.
+     * - the called Solidity function must be `payable`.
+     *
+     * _Available since v3.1._
+     */
+    function functionCallWithValue(address target, bytes memory data, uint256 value) internal returns (bytes memory) {
+        return functionCallWithValue(target, data, value, "Address: low-level call with value failed");
+    }
+
+    /**
+     * @dev Same as {xref-Address-functionCallWithValue-address-bytes-uint256-}[`functionCallWithValue`], but
+     * with `errorMessage` as a fallback revert reason when `target` reverts.
+     *
+     * _Available since v3.1._
+     */
+    function functionCallWithValue(address target, bytes memory data, uint256 value, string memory errorMessage) internal returns (bytes memory) {
+        require(address(this).balance >= value, "Address: insufficient balance for call");
+        require(isContract(target), "Address: call to non-contract");
+
+        // solhint-disable-next-line avoid-low-level-calls
+        (bool success, bytes memory returndata) = target.call{ value: value }(data);
+        return _verifyCallResult(success, returndata, errorMessage);
+    }
+
+    /**
+     * @dev Same as {xref-Address-functionCall-address-bytes-}[`functionCall`],
+     * but performing a static call.
+     *
+     * _Available since v3.3._
+     */
+    function functionStaticCall(address target, bytes memory data) internal view returns (bytes memory) {
+        return functionStaticCall(target, data, "Address: low-level static call failed");
+    }
+
+    /**
+     * @dev Same as {xref-Address-functionCall-address-bytes-string-}[`functionCall`],
+     * but performing a static call.
+     *
+     * _Available since v3.3._
+     */
+    function functionStaticCall(address target, bytes memory data, string memory errorMessage) internal view returns (bytes memory) {
+        require(isContract(target), "Address: static call to non-contract");
+
+        // solhint-disable-next-line avoid-low-level-calls
+        (bool success, bytes memory returndata) = target.staticcall(data);
+        return _verifyCallResult(success, returndata, errorMessage);
+    }
+
+    /**
+     * @dev Same as {xref-Address-functionCall-address-bytes-}[`functionCall`],
+     * but performing a delegate call.
+     *
+     * _Available since v3.4._
+     */
+    function functionDelegateCall(address target, bytes memory data) internal returns (bytes memory) {
+        return functionDelegateCall(target, data, "Address: low-level delegate call failed");
+    }
+
+    /**
+     * @dev Same as {xref-Address-functionCall-address-bytes-string-}[`functionCall`],
+     * but performing a delegate call.
+     *
+     * _Available since v3.4._
+     */
+    function functionDelegateCall(address target, bytes memory data, string memory errorMessage) internal returns (bytes memory) {
+        require(isContract(target), "Address: delegate call to non-contract");
+
+        // solhint-disable-next-line avoid-low-level-calls
+        (bool success, bytes memory returndata) = target.delegatecall(data);
+        return _verifyCallResult(success, returndata, errorMessage);
+    }
+
+    function _verifyCallResult(bool success, bytes memory returndata, string memory errorMessage) private pure returns(bytes memory) {
+        if (success) {
+            return returndata;
+        } else {
+            // Look for revert reason and bubble it up if present
+            if (returndata.length > 0) {
+                // The easiest way to bubble the revert reason is using memory via assembly
+
+                // solhint-disable-next-line no-inline-assembly
+                assembly {
+                    let returndata_size := mload(returndata)
+                    revert(add(32, returndata), returndata_size)
+                }
+            } else {
+                revert(errorMessage);
+            }
+        }
+    }
+}
+/**
+ * @title SafeBEP20
+ * @dev Wrappers around BEP20 operations that throw on failure (when the token
+ * contract returns false). Tokens that return no value (and instead revert or
+ * throw on failure) are also supported, non-reverting calls are assumed to be
+ * successful.
+ * To use this library you can add a `using SafeBEP20 for IBEP20;` statement to your contract,
+ * which allows you to call the safe operations as `token.safeTransfer(...)`, etc.
+ */
+library SafeBEP20 {
+    using SafeMath for uint256;
+    using Address for address;
+
+    function safeTransfer(IBEP20 token, address to, uint256 value) internal {
+        _callOptionalReturn(token, abi.encodeWithSelector(token.transfer.selector, to, value));
+    }
+
+    function safeTransferFrom(IBEP20 token, address from, address to, uint256 value) internal {
+        _callOptionalReturn(token, abi.encodeWithSelector(token.transferFrom.selector, from, to, value));
+    }
+
+    /**
+     * @dev Deprecated. This function has issues similar to the ones found in
+     * {IBEP20-approve}, and its usage is discouraged.
+     *
+     * Whenever possible, use {safeIncreaseAllowance} and
+     * {safeDecreaseAllowance} instead.
+     */
+    function safeApprove(IBEP20 token, address spender, uint256 value) internal {
+        // safeApprove should only be called when setting an initial allowance,
+        // or when resetting it to zero. To increase and decrease it, use
+        // 'safeIncreaseAllowance' and 'safeDecreaseAllowance'
+        // solhint-disable-next-line max-line-length
+        require((value == 0) || (token.allowance(address(this), spender) == 0),
+            "SafeBEP20: approve from non-zero to non-zero allowance"
+        );
+        _callOptionalReturn(token, abi.encodeWithSelector(token.approve.selector, spender, value));
+    }
+
+    function safeIncreaseAllowance(IBEP20 token, address spender, uint256 value) internal {
+        uint256 newAllowance = token.allowance(address(this), spender).add(value);
+        _callOptionalReturn(token, abi.encodeWithSelector(token.approve.selector, spender, newAllowance));
+    }
+
+    function safeDecreaseAllowance(IBEP20 token, address spender, uint256 value) internal {
+        uint256 newAllowance = token.allowance(address(this), spender).sub(value, "SafeBEP20: decreased allowance below zero");
+        _callOptionalReturn(token, abi.encodeWithSelector(token.approve.selector, spender, newAllowance));
+    }
+
+    /**
+     * @dev Imitates a Solidity high-level call (i.e. a regular function call to a contract), relaxing the requirement
+     * on the return value: the return value is optional (but if data is returned, it must not be false).
+     * @param token The token targeted by the call.
+     * @param data The call data (encoded using abi.encode or one of its variants).
+     */
+    function _callOptionalReturn(IBEP20 token, bytes memory data) private {
+        // We need to perform a low level call here, to bypass Solidity's return data size checking mechanism, since
+        // we're implementing it ourselves. We use {Address.functionCall} to perform this call, which verifies that
+        // the target address contains contract code and also asserts for success in the low-level call.
+
+        bytes memory returndata = address(token).functionCall(data, "SafeBEP20: low-level call failed");
+        if (returndata.length > 0) { // Return data is optional
+            // solhint-disable-next-line max-line-length
+            require(abi.decode(returndata, (bool)), "SafeBEP20: BEP20 operation did not succeed");
+        }
     }
 }
